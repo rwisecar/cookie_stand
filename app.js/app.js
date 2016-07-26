9622a79c43336349cf28c811673e25d5219be455
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-07-25 17:48:09 -0700

    version updated after class meeting

--- app.js/app.js
+++ app.js/app.js
@@ -8,22 +8,44 @@ var hours = ['6:00 a.m.', '7:00 a.m.', '8:00 a.m.', '9:00 a.m.', '10:00 a.m', '1
 var firstAndPike = {
 
   //STANDARD PROPERTIES
-  name: 'First and Pike',
-  minCustomers: 23,
-  maxCustomers: 65,
-  cookiePerSale: 6.3,
-  cookiesPerHourArray: [], //Holds cookies per hour
+  locationName: 'First and Pike',
+  minCustPerHour: 23,
+  maxCustPerHour: 65,
+  avgCookiesPerCust: 6.3,
+  custEachHourArray: [],
+  cookiesEachHourArray: [], //Holds cookies per hour
   totalDailyCookieSales: 0, //Holds sum of cookies sold per hour
-
-  //METHODS
-  avgCookies: function () {
-    for (var i = 0; i < hours.length; i++) {
-      var singleHourCookies = (Math.floor((Math.floor((Math.random() * (this.maxCustomers - this.minCustomers + 1)) + this.minCustomers)) * this.cookiePerSale));
-      this.cookiesPerHourArray.push(singleHourCookies);
-      this.totalDailyCookieSales += singleHourCookies;
-    };
-  },
 };
-firstAndPike.avgCookies(firstAndPike.minCustomers, firstAndPike.maxCustomers);
+  //METHODS
 
-//
+firstAndPike.calcCustEachHour = function() {
+  for(var i = 0; i < hours.length; i++) {
+    var singleHourCust = Math.floor(Math.random() * (this.maxCustPerHour -
+    this.minCustPerHour + 1)) + this.minCustPerHour;
+    this.custEachHourArray.push(singleHourCust);
+  };
+};
+
+firstAndPike.calcCookiesEachHour = function(){
+  this.calcCustEachHour();
+  for (var i = 0; i < hours.length; i++) {
+    var singleHourCookies = Math.ceil(this.custEachHourArray[i] * this.avgCookiesPerCust);
+    this.cookiesEachHourArray.push(singleHourCookies);
+    this.totalDailyCookieSales += singleHourCookies;
+  };
+};
+
+firstAndPike.calcCookiesEachHour();
+
+//OLD VERSION
+//   avgCookies: function () {
+//     for (var i = 0; i < hours.length; i++) {
+//       var singleHourCookies = (Math.floor((Math.floor((Math.random() * (this.maxCustomers - this.minCustomers + 1)) + this.minCustomers)) * this.cookiePerSale));
+//       this.cookiesPerHourArray.push(singleHourCookies);
+//       this.totalDailyCookieSales += singleHourCookies;
+//     };
+//   },
+// };
+// firstAndPike.avgCookies(firstAndPike.minCustomers, firstAndPike.maxCustomers);
+
+//SeaTac
